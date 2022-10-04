d55cc1082f696dece21483daa23372dac294d9c6
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2022-10-04 11:50:42 +0200

    BrowseIssueTest implemented

---
+++ src/test/java/JiraTest/BrowseIssue/BrowseIssueTest.java
@@ -0,0 +1,57 @@
+package JiraTest.BrowseIssue;
+
+import com.example.HogwartsForum.JiraTest.Model.BrowseIssue.BrowseIssueModel;
+import com.example.HogwartsForum.JiraTest.WebDriverService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class BrowseIssueTest {
+    private BrowseIssueModel browseIssueModel;
+
+    @BeforeEach
+    public void openNewTab() {
+        browseIssueModel = new BrowseIssueModel();
+        browseIssueModel.doLogin();
+    }
+
+    @AfterEach
+    public void closeWebDriver() {
+        WebDriverService.getInstance().quitWebDriver();
+    }
+
+
+
+    @Test
+    public void browseExistingIssue() {
+        browseIssueModel.openUrlWithSpecificPathAndMaximizeWindowSize("/browse/MTP-2253");
+        Assertions.assertEquals("MTP-2253", browseIssueModel.getIssueId());
+    }
+
+    @Test
+    public void checkPossibilityOfBrowsing() {
+        String expectedKey = "MTP-2245";
+        browseIssueModel.openUrlWithSpecificPathAndMaximizeWindowSize("/issues/?jql=");
+        browseIssueModel.clickSearchField();
+        browseIssueModel.writeSearchField("Jira Test Project");
+        browseIssueModel.clickSearchButton();
+        Assertions.assertTrue(browseIssueModel.waitUntilKeyIsVisible(expectedKey));
+    }
+
+    @Test
+    public void browseNonExistingIssue() {
+        browseIssueModel.openUrlWithSpecificPathAndMaximizeWindowSize("/browse/MTP-99999999999");
+        Assertions.assertEquals("You can't view this issue", browseIssueModel.getErrorMessageField());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/issueIds.csv")
+    public void browseIssueWithSpecificId(String issueId) {
+        browseIssueModel.openUrlWithSpecificPathAndMaximizeWindowSize(String.format("/browse/%s", issueId));
+        Assertions.assertDoesNotThrow(() -> browseIssueModel.getIssueId());
+        Assertions.assertEquals(issueId, browseIssueModel.getIssueId());
+    }
+}
