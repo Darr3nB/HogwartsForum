347a6a0219423eaf12f5a2a784fc9f30c6f69a32
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2022-10-20 14:18:08 +0200

    Password hashing working

--- src/main/java/com/example/HogwartsForum/Controller/RegistrationController.java
+++ src/main/java/com/example/HogwartsForum/Controller/RegistrationController.java
@@ -2,27 +2,31 @@ package com.example.HogwartsForum.Controller;
 
 import com.example.HogwartsForum.Model.HogwartsUser;
 import com.example.HogwartsForum.Model.RegistrationModel;
+import com.example.HogwartsForum.Security.PasswordAgent;
 import com.example.HogwartsForum.Services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @AllArgsConstructor
 @RequestMapping("/registration")
 public class RegistrationController {
     UserService userService;
+    PasswordAgent passwordAgent;
 
     @GetMapping
-    public String registration(){
+    public String registration() {
         return "registration";
     }
 
     @PostMapping
-    public String doRegister(@RequestBody RegistrationModel newRegistration){
-        HogwartsUser newUser = new HogwartsUser(newRegistration.getUsername(), newRegistration.getPassword(),
-                                                newRegistration.getHouse(), newRegistration.getPetType());
+    public String doRegister(@RequestBody RegistrationModel newRegistration) {
+        HogwartsUser newUser = new HogwartsUser(newRegistration.getUsername(), passwordAgent.hashPassword(newRegistration.getPassword()),
+                newRegistration.getHouse(), newRegistration.getPetType());
         userService.addUser(newUser);
 
         return "redirect:";
---
+++ src/main/java/com/example/HogwartsForum/Security/PasswordAgent.java
@@ -0,0 +1,19 @@
+package com.example.HogwartsForum.Security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class PasswordAgent {
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public String hashPassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    public Boolean passwordMatches(String hashed, String plain) {
+        return passwordEncoder.matches(plain, hashed);
+    }
+}
