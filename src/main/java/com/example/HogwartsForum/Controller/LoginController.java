7aa154a8396e569d4ec07ea8f54fe32de0433b95
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2022-10-21 12:22:27 +0200

    Username is stored in cookies now.

--- src/main/java/com/example/HogwartsForum/Controller/LoginController.java
+++ src/main/java/com/example/HogwartsForum/Controller/LoginController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 @AllArgsConstructor
 @RequestMapping("/login")
@@ -18,12 +21,16 @@ public class LoginController {
     UserService userService;
 
     @PostMapping
-    public HttpEntity<Void> doLogin(@RequestBody LoginParams loginParams) {
+    public HttpEntity<Void> doLogin(@RequestBody LoginParams loginParams, HttpServletResponse response) {
         if (!userService.validateLogin(loginParams.getUsername(), loginParams.getPassword())) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
 
         System.out.println("Valid login");
+        Cookie theCookie = new Cookie("hfUsername", loginParams.getUsername());
+        theCookie.setMaxAge(60 * 60 * 24);
+
+        response.addCookie(theCookie);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
---
+++ src/main/java/com/example/HogwartsForum/Controller/SessionController.java
@@ -0,0 +1,4 @@
+package com.example.HogwartsForum.Controller;
+
+public class SessionController {
+}
