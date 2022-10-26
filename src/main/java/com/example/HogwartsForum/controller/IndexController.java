959233833cff6d4fe226291a88ee25e13dfdbe31
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2022-10-26 11:04:55 +0200

    delete profile button added, delete-profile/{id} route created

--- src/main/java/com/example/HogwartsForum/controller/IndexController.java
+++ src/main/java/com/example/HogwartsForum/controller/IndexController.java
@@ -9,10 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -88,5 +85,13 @@ public class IndexController {
     @GetMapping(value = "/error")
     public String errorPage() {
         return "error";
+    }
+
+    @GetMapping(value = "/delete-profile/{id}")
+    public String deleteProfile(@PathVariable Integer id){
+        System.out.println("ID: " + id);
+//        userService.deleteUserById(id);
+
+        return "redirect:";
     }
 }
