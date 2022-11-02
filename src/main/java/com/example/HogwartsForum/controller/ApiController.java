86b6a769f850810e1daf2cb29ae33da02641dd33
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2022-11-02 10:17:31 +0100

    send API request with the filter data then calls loadQuestions function

--- src/main/java/com/example/HogwartsForum/controller/ApiController.java
+++ src/main/java/com/example/HogwartsForum/controller/ApiController.java
@@ -4,6 +4,9 @@ import com.example.HogwartsForum.model.Question;
 import com.example.HogwartsForum.services.QuestionService;
 import com.example.HogwartsForum.services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +34,10 @@ public class ApiController {
     @GetMapping(value = "get-all-questions")
     public List<Question> getAllQuestions(){
         return questionService.getAllQuestions();
+    }
+
+    @GetMapping(value = "get-all-questions-filtered/{title}/{description}")
+    public void getAllQuestionsFiltered(@PathVariable String title, @PathVariable String description){
+        System.out.println(title + " " + description);
     }
 }
--- src/main/resources/static/js/allQuestions.js
+++ src/main/resources/static/js/allQuestions.js
@@ -4,9 +4,25 @@ function allQuestionsPage() {
     // TODO add input fields to search for specific question
     const listedQuestionsDiv = document.querySelector("#listed-questions");
 
-    async function loadAllQuestions() {
+    function initEventListener() {
+        document.querySelector("#filter-search-button").addEventListener('click', filterQuestions);
+    }
+
+    async function filterQuestions(event) {
+        event.preventDefault();
+        const titleFieldValue = document.querySelector("#filter-title").value;
+        const descriptionFieldValue = document.querySelector("#filter-description").value;
+
+        let filteredQuestionList = await utility.apiGet(`/api/get-all-questions-filtered/${titleFieldValue}/${descriptionFieldValue}`)
+            .then(response => response.json());
+
+        loadQuestions(filteredQuestionList);
+    }
+
+    async function loadQuestions(qList = []) {
         listedQuestionsDiv.innerHTML = `<img src="/images/owl.png" alt="Picture of a cute owl." width="300" height="400">`;
-        let allQuestions = await utility.apiGet("/api/get-all-questions").then(response => response.json());
+        let allQuestions = qList.length <= 0 ?
+            await utility.apiGet("/api/get-all-questions").then(response => response.json()) : qList;
 
         if (allQuestions.length <= 0) {
             listedQuestionsDiv.innerHTML = `<div>There are no asked questions yet!</div>`;
@@ -32,7 +48,8 @@ function allQuestionsPage() {
         utility.addEventListenerToQuestions();
     }
 
-    loadAllQuestions();
+    initEventListener();
+    loadQuestions();
 }
 
 allQuestionsPage()
