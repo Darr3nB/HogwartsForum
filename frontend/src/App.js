4543317768b6c9532d312aff2988b207f37fac71
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2022-12-07 11:33:30 +0100

    Saving changes before addin react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,37 @@ import MenuLayout from "./components/MenuLayout";
 import Footer from "./components/Footer";
 
 function App() {
+    const [questions, setQuestionState] = useState([]);
+
+    // Fetch questions
+    const askedQuestions = async () => {
+        const data = await fetch(`http://localhost:8080/api/five-latest-question`).then(response => response.json());
+        // TODO In case of bad response, use state
+        setQuestionState(data);
+    };
+    // TODO useEffect maybe? If new question posted, automatically refresh the data
+
     return (
         <div className="App">
             <MenuLayout/>
+            <h1>Welcome to Hogwarts forum!</h1>
+            <button type="button">Ask a question</button>
+            {/*Put a button here that route to all questions*/}
+            <p>Find friends, chat, connect with other students, ask questions or help in studies.</p>
+            <div id="main-page-questions">
+                {questions.length <= 0
+                    ? (<p>There are no asked questions yet.</p>)
+                    : (<table>
+                        <tr>
+                            <th>Latest question(s)</th>
+                        </tr>
+                        {questions.map((question, index) => {
+                            return (<tr>
+                                <td id={"question-id-" + question.id}>{question.title + " " + question.questionText}</td>
+                            </tr>)
+                        })}
+                    </table>)}
+            </div>
             <Footer/>
         </div>
     );
--- frontend/src/components/MenuLayout.js
+++ frontend/src/components/MenuLayout.js
@@ -1,19 +1,30 @@
+import {useState} from "react";
+
 const MenuLayout = () => {
-  return (
-      <div>
-        <button>Home</button>
+    const [isLoggedIn, setLoginState] = useState(false);
+
+    const loggedOff = <div>
+        <button type="button">Home</button>
         <div>
-          <label for="username-field">Username: </label>
-          <input type="text" id="username-field" minLength="3"/>
-          <label for="password-field">Magic word: </label>
-          <input type="text" id="password-field" minLength="3"/>
-          <button type="button" id="login-button" class="nav-bar-button">Login</button>
+            <label htmlFor="username-field">Username: </label>
+            <input type="text" id="username-field" minLength="3"/>
+            <label htmlFor="password-field">Magic word: </label>
+            <input type="text" id="password-field" minLength="3"/>
+            <button type="button" id="login-button" className="nav-bar-button">Login</button>
         </div>
         <div>
-          <button type="button">Registration</button>
+            <button type="button">Registration</button>
         </div>
-      </div>
-  );
-}
+    </div>;
+
+    const loggedIn = <div>
+        <button type="button">Home</button>
+        <p>Welcome USERNAME_HERE!</p>
+        <button type="button">Logout</button>
+        <button type="button">Profile</button>
+    </div>;
+
+    return isLoggedIn ? loggedIn : loggedOff;
+};
 
 export default MenuLayout;
